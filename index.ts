f639ffaf812696a6842be39e7e3272519263349e
Felix Sabelleck <Felix.Sabelleck@Werbezeichen.de>
2020-10-16 17:34:02 +0200

    simplify function

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { colorDepth, getRGBFromPercentage, RGBValue } from './rgb.js';
 import { mandelbrot, perf } from './utils.js';
 
-const resolution = 256;
+const resolution = 512;
 
 function initGrid(container: HTMLElement) {
   const grid = document.getElementById('grid') as HTMLCanvasElement;
@@ -12,48 +12,37 @@ function initGrid(container: HTMLElement) {
   const scale = gridLength / resolution;
   grid.setAttribute('style', `transform: translate(-50%, -50%) scale(${scale})`);
 
-  const createImage = () => {
+  return (callback: (x: number, y: number) => RGBValue) => {
     const imgData = ctx.createImageData(resolution, resolution);
+    for (let i = 0; i < imgData.data.length; i += 4) {
+      const x = (i % (resolution * 4)) / 4;
+      const y  = (Math.floor(i / (resolution * 4)));
+      const [r, g, b] = callback(x, y);
 
-    return {
-      data: imgData.data,
-      setPixelAt: (i: number, [r,g,b]: RGBValue) => {
-        imgData.data[i+0] = r;
-        imgData.data[i+1] = g;
-        imgData.data[i+2] = b;
-        imgData.data[i+3] = 255;
-      },
-      paint: () => {
-        ctx.putImageData(imgData, 0, 0);
-      }
+      imgData.data[i+0] = r;
+      imgData.data[i+1] = g;
+      imgData.data[i+2] = b;
+      imgData.data[i+3] = 255;
     }
-  };
 
-  return createImage;
-}
+    ctx.clearRect(0, 0, grid.width, grid.height);
+    ctx.putImageData(imgData, 0, 0);
+  };
+};
 
 function main() {
   const container = document.getElementById('container');
-  const createImage = initGrid(container);
+  const generateImage = initGrid(container);
 
   const zoom = 2;
-
   const offset = { x: -.5 - zoom * .5, y: 0 - zoom * .5 };
 
   function generateGrid(zoom: number, offset: { x: number; y: number; }) {
-    const { data, paint, setPixelAt } = createImage();
-    for (let i = 0; i < data.length; i += 4) {
-      const res = resolution * 4;
-      const x = i % res;
-      const y = Math.floor(i / res);
-      
-      const val = mandelbrot({ r: ((x / res) * zoom) + offset.x, i: ((y / resolution) * zoom) + offset.y });
-      
-      const rgb = getRGBFromPercentage(val / colorDepth);
-      setPixelAt(i, rgb);
-    }
-  
-    paint();
+    generateImage((x, y) => {
+      const val = mandelbrot({ r: ((x / (resolution)) * zoom) + offset.x, i: ((y / (resolution)) * zoom) + offset.y });
+
+      return getRGBFromPercentage(val / colorDepth);
+    });
   }
 
   perf('generate-grid', () => generateGrid(zoom, offset));
